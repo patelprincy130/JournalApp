fd7df31930ca0dd5a9c5e67ce862071ec2b8fa8f
Princy Patel <patelprincy130@gmail.com>
2026-02-03 06:31:27 -0500

    added component

--- src/main/java/com/example/journalApp/service/UserService.java
+++ src/main/java/com/example/journalApp/service/UserService.java
@@ -3,10 +3,12 @@ package com.example.journalApp.service;
 import com.example.journalApp.entity.UserEntity;
 import com.example.journalApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public class UserService {
 
     @Autowired
